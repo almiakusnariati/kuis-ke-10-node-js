5cebca1d665847671c0f42c5f30b4c3dc8a425d9
almiakusnariati <almiakusnariati@gmail.com>
2023-10-17 20:38:51 +0700

    first

---
+++ jwb1.js
@@ -0,0 +1,10 @@
+import { tambah,kali,phi } from "./mathOperation";
+
+
+const hasilTambah = tambah(9, 8);
+const hasilKali = kali(8, 7);
+
+
+console.log('hasil penambahan:', hasilTambah);
+console.log('hasil perkalian :', hasilKali);
+console.log('hasil phi :', phi);
---
+++ mathOperation.js
@@ -0,0 +1,7 @@
+export function tambah(a,b){
+    return a+b;
+}
+export function kali(a,b){
+    return a*b;
+}
+export const Phi = 3.14159;
